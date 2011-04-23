213e28f295c770ba7be173a35e5914b0e7d1fbdb
Paul Chobert <paul@chobert.fr>
2011-04-23 10:04:51 +0200

    RESTful Batiments.

--- app/controllers/batiments_controller.rb
+++ app/controllers/batiments_controller.rb
@@ -1,17 +1,38 @@
 class BatimentsController < ApplicationController
+  respond_to :html, :json, :xml
+  
   def index
-    if params[:categorie_id] && @categorie = Categorie.find(params[:categorie_id])
-      @campus = Campus.find(params[:campus_id])
-      @batiments = Batiment.where(:campus_id => @campus, :categorie_id => @categorie)
-    else
-      @campus = Campus.find(params[:campus_id])
-      @batiments = @campus.batiments
+    if params[:campus_id] && @campus = Campus.find(params[:campus_id])
+      if params[:categorie_id] && @categorie = Categorie.find(params[:categorie_id])
+        @batiments = Batiment.where(:campus_id => @campus, :categorie_id => @categorie)
+      else
+        @batiments = @campus.batiments
+      end
     end
+    respond_with @batiment
   end
   
   def show
     @batiment = Batiment.find(params[:id])
     @entrees = @batiment.entrees
     @services = @batiment.services
+    respond_with @batiment
+  end
+  
+  def create
+    @campus = Campus.find(params[:campus_id])
+    respond_with [@campus, @batiment = @campus.batiments.create(params[:batiment])]
+  end
+  
+  def update
+    @batiment = Batiment.find(params[:id])
+    @batiment.update_attributes(params[:batiment])
+    respond_with [@batiment.campus, @batiment]
+  end
+  
+  def destroy
+    @batiment = Batiment.find(params[:id])
+    @campus = @batiment.campus
+    respond_with [@campus, @batiment.destroy]
   end
 end
