a0ae16aebea031a8c9be033ad07402bd1d57eb7c
Paul Chobert <paul@chobert.fr>
2011-04-23 10:41:09 +0200

    Validateurs de Batiment.

--- app/models/batiment.rb
+++ app/models/batiment.rb
@@ -4,4 +4,12 @@ class Batiment < ActiveRecord::Base
   has_many :bornes
   has_many :entrees
   has_many :services
+  
+  validates :nom,           :presence => true, :uniqueness => true
+  validates :url_photo,     :presence => true
+  validates :adresse,       :presence => true
+  validates :code_postale,  :presence => true
+  validates :ville,         :presence => true
+  validates :longitude,     :presence => true
+  validates :latitude,      :presence => true
 end
