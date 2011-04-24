2bc8300f8b951b159dc4c93be93934e651f4dd2c
Paul Chobert <paul@chobert.fr>
2011-04-24 18:44:12 +0200

    RESTful Entrees.

---
+++ app/controllers/entrees_controller.rb
@@ -0,0 +1,35 @@
+class EntreesController < ApplicationController
+  respond_to :html, :json, :xml
+
+  def index
+    if params[:batiment_id] && @batiment = Batiment.find(params[:batiment_id])
+      @entrees = @batiment.entrees
+    else
+      @entrees = Entree.all
+    end
+    respond_with @entrees
+  end
+
+  def show
+    @entree = Entree.find(params[:id])
+    respond_with @entree
+  end
+
+  def create
+    @batiment = Batiment.find(params[:batiment_id])
+    respond_with(@entree = @batiment.entrees.create(params[:entree]))
+  end
+
+  def update
+    @entree = Entree.find(params[:id])
+    @entree.update_attributes(params[:entree])
+    respond_with [@entree.batiment.campus, @entree.batiment, @entree]
+  end
+
+  def destroy
+    @entree = Entree.find(params[:id])
+    @batiment = @entree.batiment
+    @campus = @batiment.campus
+    respond_with [@campus, @batiment, @entree.destroy]
+  end
+end
---
+++ test/unit/helpers/entrees_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class EntreesHelperTest < ActionView::TestCase
+end
