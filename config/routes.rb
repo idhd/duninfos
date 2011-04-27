1579144dfad070f1949a6c754cdd5d04bbe2b73c
Paul Chobert <paul@chobert.fr>
2011-04-27 14:48:23 +0200

    Ajouter le contrôleur d'administration.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Duninfos::Application.routes.draw do
     end
   end
   
+  match '/admin/' => "admin#new"
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
