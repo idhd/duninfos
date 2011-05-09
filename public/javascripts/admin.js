c8965c6b6945e2f0187a7797d8f86cfd981ab26a
BEE Lucas <pouulet@gmail.com>
2011-05-09 11:32:54 +0200

    modification du formulaire

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,57 +1,86 @@
 $(document).ready(function(){
   $(".all").hide();
-
+  var choix = "";
     $("label[for='choix-campus']").click(function(){
       $(".all").hide();
       $(".all").attr('disabled', 'disabled');
       $(".campus").show(); 
-      $(".campus").attr('disabled', '');
-      console.log($(".campus"));
-      $(".campus").each(function(e) {
-        e.attr('name','campus['+e.attr('name')+']');
-      });
-
-      //var id = $(".all").attr("id");
-      //var name = $("").attr("name");
-
-      //$("."+id).attr('name', id+'['+name+']');
+      $(".campus").attr('disabled', '');     
+      choix = "campus";
     });      
     $("label[for='choix-batiment']").click(function(){
       $(".all").hide();
       $(".all").attr('disabled', 'disabled');
       $(".batiment").show(); 
       $(".batiment").attr('disabled', '');     
+      choix = "batiment";
     });
     $("label[for='choix-salle']").click(function(){
       $(".all").hide();   
       $(".all").attr('disabled', 'disabled');
       $(".salle").show();   
       $(".salle").attr('disabled', '');
+      choix = "salle";
     });
     $("label[for='choix-entree']").click(function(){
       $(".all").hide();
       $(".all").attr('disabled', 'disabled');
       $(".entree").show();      
       $(".entree").attr('disabled', '');
+      choix = "entree";
     });  
     $("label[for='choix-borne']").click(function(){
       $(".all").hide();  
       $(".all").attr('disabled', 'disabled');
       $(".borne").show();      
       $(".borne").attr('disabled', '');
+      choix = "borne";
     });
     $("label[for='choix-service']").click(function(){
       $(".all").hide();
       $(".all").attr('disabled', 'disabled');
       $(".service").show();      
       $(".service").attr('disabled', '');
+      choix = "service";
     });
     $("label[for='choix-categorie']").click(function(){
       $(".all").hide();   
       $(".all").attr('disabled', 'disabled');
       $(".categorie").show();
       $(".categorie").attr('disabled', '');
+      choix = "categorie";
     });
+    $("form").submit(function() {
+      $("."+choix).each(function() {
+        $(this).attr('name',choix+'['+$(this).attr('name')+']');
+      });
+      
+      var batiment_id = $("#select-batiment").attr('value');
+      var campus_id =  $("#select-campus").attr('value');
+      
+      if(choix == "batiment") {
+        $("form").attr('action','campuses/'+campus_id+'/batiments');
+        }
+      else if(choix == "campus") {
+        $("form").attr('action','campuses/');
+        }
+      else if(choix == "salle") {
+        $("form").attr('action','campuses/'+campus_id+'/batiments/'+batiment_id+'/salles');
+        }
+      else if(choix == "entree") {
+        $("form").attr('action','campuses/'+campus_id+'/batiments/'+batiment_id+'/entrees');
+        }
+      else if(choix == "borne") {
+        $("form").attr('action','campuses/'+campus_id+'/batiments/'+batiment_id+'/bornes');
+        }
+      else if(choix == "service") {
+        $("form").attr('action','campuses/'+campus_id+'/batiments/'+batiment_id+'/services');
+        }
+      else if(choix == "categorie") {
+      }                                   
+    });
+    
+
 });
 /* 
 disabled
