8f3ee05e04f0e7807378604539c071bd0c4d82cf
Sebastien <sebstp@gmail.com>
2011-05-24 14:13:18 +0200

    Pilule : lien vers carte Bouton désactivé sur toutes les pages sauf sur l'index des batiments et sur le show d'un batiment

--- public/javascripts/carte.js
+++ public/javascripts/carte.js
@@ -4,27 +4,26 @@ $(document).ready(function () {
   var url = window.location.pathname.substr( 0, window.location.pathname.length-6 );
   
   // On récupère l'objet JSON correspondant à l'adresse précédente
-	$.getJSON( url,
-	  function(data) {
+	$.getJSON( url, function(data) {
 	  
-	    // Si l'objet JSON récupéré n'est pas un tableau de batiments, on le transforme en tableau
-	    if (!$.isArray(data)) data = new Array(data);
-	      
-	    // Pour chaque batiment du tableau
-      $.each(data, function(key, val) {
+    // Si l'objet JSON récupéré n'est pas un tableau de batiments, on le transforme en tableau
+    if (!$.isArray(data)) data = new Array(data);
       
-        // On positionne un marqueur
-        $('#map').gmap3(
-          { action: 'addMarker',
-            latLng: [val.batiment.latitude, val.batiment.longitude],
-            infowindow:
-            {
-              options:{
-                content: val.batiment.nom
-              }
-            }
-          });
-        });
-    });
-    
+    // Pour chaque batiment du tableau
+    $.each(data, function(key, val) {
+
+      // On positionne un marqueur pour ce batiment
+      $('#map').gmap3({
+        action: 'addMarker',
+        latLng: [val.batiment.latitude, val.batiment.longitude],
+        infowindow:
+        {
+          options:
+          {
+            content: val.batiment.nom
+          }
+        }
+      });
+    });   
+  });
 });
