e1303a1a067ec68c58d12acf3762fab297b725b9
Paul Chobert <paul@chobert.fr>
2011-05-28 15:11:52 +0200

    Suppression des méthodes next_entree et previous_entree remplacées par act_as_list.

--- app/models/entree.rb
+++ app/models/entree.rb
@@ -1,13 +1,4 @@
 class Entree < ActiveRecord::Base
   belongs_to :batiment
   acts_as_list :scope => :batiment
-
-def next_entree
-  Entree.where(:position => position + 1).first  
-end
-
-def previous_entree
-  Entree.where(:position => position - 1).first  
-end
-
 end
