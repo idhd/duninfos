80087aa4361226a57f78628bebf6a0995dbda68b
Paul Chobert <paul@chobert.fr>
2011-05-28 22:37:17 +0200

    Refactoring de l'édition de campus.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def form_geolocation_fields(form, adresse = true)
+    concat (javascript_include_tag 'geolocation.js')
+    content_tag :div, nil, :class => 'geolocation' do
+      concat (raw "<input type='button' value='me localiser' />")
+      concat (content_tag :div, 'data-role' => 'fieldcontain' do
+        concat form.label :latitude
+        concat form.text_field :latitude, :class => 'latitude'
+      end)
+      concat (content_tag :div, 'data-role' => 'fieldcontain' do
+        concat form.label :longitude
+        concat form.text_field :longitude, :class => 'longitude'
+      end)
+      concat (content_tag :p, nil, :class => 'adresse')
+    end
+  end
 end
