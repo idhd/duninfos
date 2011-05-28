f74a7a9f8212ff0bd7cce659d3a1a57ffa09f84f
Paul Chobert <paul@chobert.fr>
2011-05-28 17:13:04 +0200

    Permettre l'upload de photos pour les entrées. Modification des fixtures d'entrées.

---
+++ db/migrate/20110528144026_add_attachment_photo_to_entree.rb
@@ -0,0 +1,15 @@
+class AddAttachmentPhotoToEntree < ActiveRecord::Migration
+  def self.up
+    add_column :entrees, :photo_file_name, :string
+    add_column :entrees, :photo_content_type, :string
+    add_column :entrees, :photo_file_size, :integer
+    add_column :entrees, :photo_updated_at, :datetime
+  end
+
+  def self.down
+    remove_column :entrees, :photo_file_name
+    remove_column :entrees, :photo_content_type
+    remove_column :entrees, :photo_file_size
+    remove_column :entrees, :photo_updated_at
+  end
+end
