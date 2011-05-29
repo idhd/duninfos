a08170eca542eedb00bbc6b7c3b29198643fa982
Paul Chobert <paul@chobert.fr>
2011-05-29 23:34:04 +0200

    Ajouter l'icone aux catégories.

--- app/models/categorie.rb
+++ app/models/categorie.rb
@@ -1,2 +1,8 @@
 class Categorie < ActiveRecord::Base
+  has_attached_file :icon,
+    :styles => { :default => "50x50" },
+    :url => "/images/categories/:id.:extension",
+    :path => ":rails_root/public/images/categories/:id.:extension"
+  validates_attachment_size :icon, :less_than => 5.megabytes  
+  validates_attachment_content_type :icon, :content_type => ['image/jpeg', 'image/png']
 end
